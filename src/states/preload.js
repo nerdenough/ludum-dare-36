2c77e8d08f7ed2519df92d48ea94254cef4afd2d
Brendan Goodenough <brendan@goodenough.nz>
2016-08-30 08:58:36 +1200

    Fix resources for gh-pages

--- src/states/preload.js
+++ src/states/preload.js
@@ -7,35 +7,35 @@ class Preload {
     this.loadingText = game.add.text(32, 32, 'Loading...', {fill: '#fff'});
 
     // Sprites
-    game.load.spritesheet('player', '/assets/entities/player/player.png', 34, 31);
-    game.load.spritesheet('artifact', '/assets/entities/artifact/artifact.png', 41, 34);
-    game.load.image('artifact-module', '/assets/entities/artifact/artifact-module.png');
-    game.load.image('bullet', '/assets/entities/bullets/bullet.png');
-    game.load.image('beam', '/assets/entities/bullets/beam.png');
-    game.load.image('platform', '/assets/platforms/platform.png');
-    game.load.image('background', '/assets/background.png');
+    game.load.spritesheet('player', './assets/entities/player/player.png', 34, 31);
+    game.load.spritesheet('artifact', './assets/entities/artifact/artifact.png', 41, 34);
+    game.load.image('artifact-module', './assets/entities/artifact/artifact-module.png');
+    game.load.image('bullet', './assets/entities/bullets/bullet.png');
+    game.load.image('beam', './assets/entities/bullets/beam.png');
+    game.load.image('platform', './assets/platforms/platform.png');
+    game.load.image('background', './assets/background.png');
 
     // Map
-    game.load.tilemap('map', '/assets/map/map.json', null,
+    game.load.tilemap('map', './assets/map/map.json', null,
       Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', '/assets/map/tiles.png');
-    game.load.image('platform', '/assets/platforms/platform.png');
-    game.load.image('platform-left', '/assets/platforms/platform-left.png');
-    game.load.image('platform-right', '/assets/platforms/platform-right.png');
-    game.load.image('moving-platform-right', '/assets/platforms/moving-platform-right.png');
-    game.load.image('moving-platform-left', '/assets/platforms/moving-platform-left.png');
-    game.load.image('moving-platform', '/assets/platforms/moving-platform.png');
-    game.load.image('activated', '/assets/objects/activator-activated.png');
-    game.load.image('deactivated', '/assets/objects/activator-deactivated.png');
+    game.load.image('tiles', './assets/map/tiles.png');
+    game.load.image('platform', './assets/platforms/platform.png');
+    game.load.image('platform-left', './assets/platforms/platform-left.png');
+    game.load.image('platform-right', './assets/platforms/platform-right.png');
+    game.load.image('moving-platform-right', './assets/platforms/moving-platform-right.png');
+    game.load.image('moving-platform-left', './assets/platforms/moving-platform-left.png');
+    game.load.image('moving-platform', './assets/platforms/moving-platform.png');
+    game.load.image('activated', './assets/objects/activator-activated.png');
+    game.load.image('deactivated', './assets/objects/activator-deactivated.png');
 
     // HUD
-    game.load.image('avatar', '/assets/hud/avatar.png');
-    game.load.image('winner', '/assets/screens/winner.png');
-    game.load.image('gameover', '/assets/screens/gameover.png');
+    game.load.image('avatar', './assets/hud/avatar.png');
+    game.load.image('winner', './assets/screens/winner.png');
+    game.load.image('gameover', './assets/screens/gameover.png');
 
     // Audio
-    game.load.audio('jump', '/assets/sounds/jump.wav');
-    game.load.audio('shoot', '/assets/sounds/shoot.wav');
+    game.load.audio('jump', './assets/sounds/jump.wav');
+    game.load.audio('shoot', './assets/sounds/shoot.wav');
 
     game.load.start();
   }
